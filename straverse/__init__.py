3d5009679b68ca95f5350857e0610a148b6430cc
T.B <infy@cybershade.org>
2017-09-12 02:38:48 +0200

    Renamed some arguments, added file splicing idea

--- straverse/__init__.py
+++ straverse/__init__.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 import argparse
 import sys
+import os
 import straverse.straverse as straverse
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Cross-platform static file signature scanner")
-    parser.add_argument("-c", "--config", help="Configuration file",
+    parser.add_argument("-i", "--input", help="Configuration file",
                         type=argparse.FileType('r'), default="config.json")
     parser.add_argument("-o", "--output", help="Output file",
                         type=argparse.FileType('w'), default=sys.stdout)
     parser.add_argument("-q", "--quiet", help="Do not display any output", action="store_true")
     parser.add_argument("-v", "--verify", help="Verify against a single signature", type=str)
-    parser.add_argument("-t", "--transform",
-                        help="Transform a signature into straverse compatible signature")
+    parser.add_argument("-c", "--convert",
+                        help="Converts a signature into straverse compatible signature")
+    parser.add_argument("-t", "--threads", help="Number of threads to run", type=int, default=4)
     parser.add_argument("file", nargs="?", type=argparse.FileType('rb'))
     args = parser.parse_args()
 
@@ -22,18 +25,26 @@ def main():
         sys.exit(1)
 
     # If -v or -t aren't present and file isn't as well, error
-    if not (args.verify or args.transform) and not args.file:
+    if not (args.verify or args.convert) and not args.file:
         parser.print_help()
         sys.exit(1)
 
+    # Set quietness
+    if args.quiet:
+        sys.stdout = open(os.devnull, "a")
+        sys.stderr = open(os.devnull, "a")
+
+    # Instantiate STraverse
+    s = straverse.STraverse(args.threads)
+
     if args.verify:
+        # s.load_file(...)
         pass
         return
-    if args.transform:
+    if args.convert:
         pass
         return
 
-    s = straverse.STraverse(args.quiet)
     s.load_file(args.file)
-    # Process file
+    s.process()
     s.close_file()
--- straverse/straverse.py
+++ straverse/straverse.py
@@ -4,16 +4,45 @@ import os
 
 
 class STraverse(object):
+    mmap = None
+    fp = None
+
     """ Initializes STraverse """
-    def __init__(self, quiet):
-        self.quiet = quiet
+    def __init__(self, threads: int) -> None:
+        self.threads = threads
 
     """ Memory maps the file """
-    def load_file(self, file):
+    def load_file(self, file) -> None:
+        self.fp = file
         if os.name == 'nt':
             self.mmap = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
         else:
             self.mmap = mmap.mmap(file.fileno(), 0, prot=mmap.PROT_READ)
 
-    def close_file(self):
+    def close_file(self) -> None:
+        """ Closes the memory mapped file """
         self.mmap.close()
+
+    def process(self) -> None:
+        """ Processes the file. """
+        # Determine the file size and the number of threads needed
+        file_size = os.fstat(self.fp.fileno()).st_size
+        chunk_size = file_size / self.threads
+        print("File size: %s. Chunk size: %s. Using %d threads..." %
+              (self.sizeof_fmt(file_size), self.sizeof_fmt(chunk_size), self.threads))
+
+        for i in range(self.threads):
+            print("%d. From %d to %d" % (i, chunk_size*i, chunk_size*(i+1)))
+
+    @staticmethod
+    def sizeof_fmt(num: int, suffix: str = 'B') -> str:
+        """ https://stackoverflow.com/a/1094933
+        :param num: number to transform
+        :param suffix: the suffix to show
+        :return: human readable string
+        """
+        for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+            if abs(num) < 1024.0:
+                return "%3.1f%s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.1f%s%s" % (num, 'Yi', suffix)
